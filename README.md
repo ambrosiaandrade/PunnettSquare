d9c6f57809dafdbedcc885f018581c1226c7614e
Ambrósia Andrade <70069811+dev-ambro@users.noreply.github.com>
2021-01-13 16:55:14 -0300

    First functional version of the PunnettSquare
    Works with one trace and in a static way.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Quadro de Punnett
 
-Site em desenvolvimento para auxiliar os estudos relacionado à genética. Sendo portanto, um site com fins educativos que poderá ser usado para esse fim.
+Desenvolvido para fins didáticos, este site tem o objetivo de auxiliar os estudos da genética. Gera o quadro de Punnett com um traço.
+
+-------------------------------------------------------------
+
+# Punnett Square
+
+Developed for educational pourposes, this site aims to assist genetic studies. Generates the Punnett square with one trace.
+
+-------------------------------------------------------------
+
+*Author: Ambrósia Andrade
+Year: 2021
+GitHub: github.com/dev-ambro*
--- js/script.js
+++ js/script.js
@@ -1,56 +1,99 @@
-window.addEventListener("load", start);
-// quando a página carregar execute a função start
+/* 
+Author: Ambrósia Andrade
+Year: 2021
+GitHub: https://github.com/dev-ambro
+*/
 
-function start(){
-    
+// Global variables
 
-}
-
-// Essas variáveis precisam ser globais para que eu possa usar em outra função
 var text1_1, text2_1;
+var ArrayResult = [];
+var array1_1,array2_1;
+var showMessage = document.querySelector("#message");
+showMessage.style.visibility = "hidden";
 
-// Função invocada quando o botão "Definir traços" é clicado
-// btn_setTraces
+// This funcion is active when the button "Gerar" is pressed
+
 function getValueSelected(){
     
-    console.log("Traços definidos!");
-
-    // Parente 1
+    // Parent 1
     var parent1_1 = document.querySelector("#p1t1");
     var value1_1 = parent1_1.selectedIndex;
     text1_1 = parent1_1.options[value1_1].innerText;
-    console.log("Parente 1: " + text1_1);
     
-    // Parente 2    
+    // Parent 2    
     var parent2_1 = document.querySelector("#p2t1");
     var value2_1 = parent2_1.selectedIndex;
     text2_1 = parent2_1.options[value2_1].innerText;
-    console.log("Parente 2: " + text2_1);
+
+    ArrayResult = [];
+
+    showMessage.style.visibility = "visible";
+
+    setValues();
 }
 
-// Função invocada quando o botão "Gerar" é clicado
-// btn_generate
-
-var ArrayResult = [];
-var teste1, teste2;
-var array1_1,array2_1;
+// This function feeds the array that will be printed on the table
 
 function setValues(){
-     array1_1 = text1_1.split("");
-     array2_1 = text2_1.split("");
+    
+    array1_1 = text1_1.split("");
+    array2_1 = text2_1.split("");
 
-    teste1 = array1_1[0] + array2_1[0];
-    teste2 = array1_1[1] + array2_1[1];
-    //console.log(array2_1);
+    // The array with the results is created in a static way
 
-    for(var i = 0; i <= text1_1.lenght; i++){
-        ArrayResult[i] = [array1_1[0] + array2_1[0]];
-    }
-    console.log("Array:" + ArrayResult);
+    ArrayResult.push(array1_1[0] + array2_1[0]);
+    ArrayResult.push(array1_1[0] + array2_1[1]);
+    ArrayResult.push(array1_1[1] + array2_1[0]);
+    ArrayResult.push(array1_1[1] + array2_1[1]);
 
+    printTable();
 }
 
+// This function writes on the webpage the results of the punnett square
 
+function printTable(){
+    var table = document.querySelector("#resultTable");
+    table.innerHTML = 
+    `<table>
+    <tr>
+        <td>${ArrayResult[0]}</td>
+        <td>${ArrayResult[1]}</td>
+    </tr>    
+    <tr>
+        <td>${ArrayResult[2]}</td>
+        <td>${ArrayResult[3]}</td>
+    </tr>    
+    </table>`;
 
-var buttonTrace = document.querySelector("#btn_trace");
-var buttonGenerate = document.querySelector("#btn_generate");
+    calculateTraces();
+}
+
+// This function calculates the percentage of the traces and write it on the webpage
+
+function calculateTraces(){
+    var count_AA = 0, count_Aa = 0, count_aa = 0;
+    for (var i = 0; i < 4; i++){
+        if (ArrayResult[i] == "AA") {
+            count_AA++;
+        } 
+        if ((ArrayResult[i] == "Aa") || (ArrayResult[i] == "aA")) {
+            count_Aa++;
+        }; 
+        if (ArrayResult[i] == "aa") {
+            count_aa++;
+        }; 
+    }
+    
+    var calculus = document.querySelector("#resultCalc");
+    calculus.textContent = 
+    "AA: " + (count_AA/4)*100 + "% "
+    + "\nAa: " + (count_Aa/4)*100 + "% "
+    + "\naa: " + (count_aa/4)*100 + "%";
+    
+    /*
+    console.log("Porcentagem!\nAA: " + (count_AA/4)*100 + "%"
+    + "\nAa: " + (count_Aa/4)*100 + "%"
+    + "\naa: " + (count_aa/4)*100 + "%");
+    */
+}
